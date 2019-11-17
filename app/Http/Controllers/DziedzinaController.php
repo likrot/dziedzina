e772b877b2f9b2103c66c0b547a2a1ea2f7eec34
likrot <bramish@gmail.com>
2019-11-17 22:44:27 +0100

    init

---
+++ app/Http/Controllers/DziedzinaController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class Dziedzina extends Controller
+{
+    public function addMaterial () 
+    {
+        //add material
+    }
+}
